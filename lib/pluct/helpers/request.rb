447a1769f0b1b2c95acaf64784d82d65c9198ff2
Alberto Leal <albertonb@gmail.com>
2013-06-10 14:32:54 -0300

    Adding http methods.

--- lib/pluct/helpers/request.rb
+++ lib/pluct/helpers/request.rb
@@ -16,6 +16,49 @@ module Pluct
         raise_exception(e)
       end
 
+      def head(url, *opts)
+        options = Hash[*opts]
+        resource = RestClient::Resource.new(url)
+        resource.head(DEFAULT_HEADERS.merge(options))
+      rescue RestClient::Exception => e
+        raise_exception(e)
+      end
+
+      def delete(url, *opts)
+        options = Hash[*opts]
+        resource = RestClient::Resource.new(url)
+        resource.delete(DEFAULT_HEADERS.merge(options))
+      rescue RestClient::Exception => e
+        raise_exception(e)
+      end
+
+      def post(url, *opts)
+        data, options = *opts
+        options = Hash[*opts] if options
+        resource = RestClient::Resource.new(url)
+        resource.post(data, DEFAULT_HEADERS.merge(options))
+      rescue RestClient::Exception => e
+        raise_exception(e)
+      end
+
+      def put(url, *opts)
+        data, options = *opts
+        options = Hash[*opts] if options
+        resource = RestClient::Resource.new(url)
+        resource.put(data, DEFAULT_HEADERS.merge(options))
+      rescue RestClient::Exception => e
+        raise_exception(e)
+      end
+
+      def patch(url, *opts)
+        data, options = *opts
+        options = Hash[*opts] if options
+        resource = RestClient::Resource.new(url)
+        resource.patch(data, DEFAULT_HEADERS.merge(options))
+      rescue RestClient::Exception => e
+        raise_exception(e)
+      end
+
       private
       def raise_exception(exception)
         case exception.http_code
