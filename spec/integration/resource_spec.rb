9560be4fad4cc4f10508b415e71a1e894e64354a
Alberto Leal <albertonb@gmail.com>
2013-06-27 14:43:19 -0300

    Adding vcr to integration tests.

--- spec/integration/resource_spec.rb
+++ spec/integration/resource_spec.rb
@@ -4,7 +4,7 @@ describe Pluct::Resource, :integration do
 
   let(:resource){ Pluct::Resource.new('http://localhost:8888') }
 
-  it 'creates a new instance' do
+  it 'creates a new instance', :vcr, cassette_name: 'integration/creates_new_app' do
     app = resource.collection({context_name: 'baas', collection_name: 'apps'}).create({name: 'my_app', description: 'app_desc'})
 
     expect(app.response.code).to eq 201
@@ -12,12 +12,12 @@ describe Pluct::Resource, :integration do
     expect(app.data).to be_empty
   end
 
-  it 'expands uri template' do
+  it 'expands uri template', :vcr, cassette_name: 'integration/expands_uri' do
     collection = resource.collection({context_name: 'baas', collection_name: 'apps'})
     expect(collection.uri).to eq 'http://localhost:8888/baas/apps'
   end
 
-  it 'raises 404 for url not found' do
+  it 'raises 404 for url not found', :vcr, cassette_name: 'integration/page_not_found' do
     expect {resource.resource({context_name: 'baas', collection_name: 'apps', resource_id: 'invalid'}) }.to raise_error(Pluct::Errors::UrlNotFound)
   end
 end
